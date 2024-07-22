bf814987994f1bfa03376d181e0f12e167b7f43f
Andy Ming <ami162@uclive.ac.nz>
2024-07-22 22:14:20 +1200

    Setup test suite

--- target/src/ADC_read.c
+++ target/src/ADC_read.c
@@ -12,15 +12,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
 #include "driverlib/adc.h"
-#include "driverlib/pwm.h"
-#include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/debug.h"
-#include "utils/ustdlib.h"
 #include "circBufT.h"
 
 //*****************************************************************************
--- tests/test_fff.c
+++ tests/test_fff.c
@@ -7,10 +7,11 @@ DEFINE_FFF_GLOBALS;
 
 /* Static variables */
 uint32_t dummy_var = 3;
+char dummy_char = 'f';
 
 /* Mocks */
 FAKE_VOID_FUNC(mocked_void_function);
-FAKE_VOID_FUNC(mocked_arg_function, uint32_t*, uint32_t);
+FAKE_VOID_FUNC(mocked_arg_function, uint32_t*, uint32_t, char);
 FAKE_VALUE_FUNC(uint32_t, mocked_value_function);
 
 /* Unity setup/teardown*/
@@ -35,7 +36,7 @@ void foo(void)
 
 void bar(void)
 {
-    mocked_arg_function(&dummy_var, dummy_var);
+    mocked_arg_function(&dummy_var, dummy_var, dummy_char);
 }
 
 uint32_t baz(void)
@@ -55,6 +56,7 @@ void test_bar_passes_dummy_var_as_pinter_and_value_args(void)
     bar();
     TEST_ASSERT_EQUAL_PTR(&dummy_var, mocked_arg_function_fake.arg0_val);
     TEST_ASSERT_EQUAL(dummy_var, mocked_arg_function_fake.arg1_val);
+    TEST_ASSERT_EQUAL(dummy_char, mocked_arg_function_fake.arg2_val);
 }
 
 void test_baz_returns_the_return_value_of_mocked_value_function(void)
