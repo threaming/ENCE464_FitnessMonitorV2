32c7ea32c4d5e6166b084bab3faea3963f619c51
Andy Ming <ami162@uclive.ac.nz>
2024-08-12 12:59:06 +1200

    removed unnecessary dependencies

--- target/src/accl_manager.c
+++ target/src/accl_manager.c
@@ -8,28 +8,17 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "inc/hw_ints.h"
 #include "driverlib/gpio.h"
 #include "driverlib/i2c.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
 #include "driverlib/debug.h"
 #include "driverlib/pin_map.h"
-#include "utils/ustdlib.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "utils/ustdlib.h"
-#include "buttons4.h"
 #include "acc.h"
 #include "i2c_driver.h"
 #include "circBufV.h"
-
-
 #include "accl_manager.h"
-
 
 
 //********************************************************
