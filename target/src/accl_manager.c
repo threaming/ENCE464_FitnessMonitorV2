19c55b211f73737782bc9c8ac1869e33461df235
Andy Ming <ami162@uclive.ac.nz>
2024-08-13 16:18:50 +1200

    changed accl_manager to use i2c hal adaptation

--- target/src/accl_manager.c
+++ target/src/accl_manager.c
@@ -16,7 +16,7 @@
 #include "driverlib/debug.h"
 #include "driverlib/pin_map.h"
 #include "acc.h"
-#include "i2c_driver.h"
+#include "hal/i2c_hal.h"
 #include "circBufV.h"
 #include "accl_manager.h"
 
@@ -93,26 +93,7 @@ void initAcclChip(void)
 {
     char    toAccl[] = {0, 0};  // parameter, value
 
-    /*
-    * Enable I2C Peripheral
-    */
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
-    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
-
-    /*
-    * Set I2C GPIO pins
-    */
-    GPIOPinTypeI2C(I2CSDAPort, I2CSDA_PIN);
-    GPIOPinTypeI2CSCL(I2CSCLPort, I2CSCL_PIN);
-    GPIOPinConfigure(I2CSCL);
-    GPIOPinConfigure(I2CSDA);
-
-    /*
-    * Setup I2C
-    */
-    I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), true);
+    i2c_hal_register(I2C_ID_1);
 
     GPIOPinTypeGPIOInput(ACCL_INT2Port, ACCL_INT2);
 
@@ -121,31 +102,31 @@ void initAcclChip(void)
     // set +-16g, 13 bit resolution, active low interrupts
     toAccl[0] = ACCL_DATA_FORMAT;
     toAccl[1] = (ACCL_RANGE_16G | ACCL_FULL_RES);
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 
     toAccl[0] = ACCL_PWR_CTL;
     toAccl[1] = ACCL_MEASURE;
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 
     toAccl[0] = ACCL_BW_RATE;
     toAccl[1] = ACCL_RATE_100HZ;
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 
     toAccl[0] = ACCL_INT;
     toAccl[1] = 0x00;       // Disable interrupts from accelerometer.
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 
     toAccl[0] = ACCL_OFFSET_X;
     toAccl[1] = 0x00;
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 
     toAccl[0] = ACCL_OFFSET_Y;
     toAccl[1] = 0x00;
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 
     toAccl[0] = ACCL_OFFSET_Z;
     toAccl[1] = 0x00;
-    I2CGenTransmit(toAccl, 1, WRITE, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, toAccl, 1, I2C_WRITE, ACCL_ADDR);
 }
 
 
@@ -158,7 +139,7 @@ vector3_t getAcclData(void)
     uint8_t bytesToRead = 6;
 
     fromAccl[0] = ACCL_DATA_X0;
-    I2CGenTransmit(fromAccl, bytesToRead, READ, ACCL_ADDR);
+    i2c_hal_transmit(I2C_ID_1, fromAccl, bytesToRead, I2C_READ, ACCL_ADDR);
 
     acceleration.x = (fromAccl[2] << 8) | fromAccl[1]; // Return 16-bit acceleration readings.
     acceleration.y = (fromAccl[4] << 8) | fromAccl[3];
